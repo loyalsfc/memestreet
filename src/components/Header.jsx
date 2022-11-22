0b881cda3724dded0095edf228e6b279e8ad6d0a
Olumide Bambe <loyalsfc@gmail.com>
2022-11-22 10:46:59 +0100

    added tokenomics

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,12 @@ import Navigation from './Navigation'
 
 function Header() {
   function showMenu(){
+    console.log('hi')
     document.querySelector('.menu').classList.toggle('hidden');
   }
 
   return (
-    <header className='px-2 relative'>
+    <header className='px-2 relative z-10'>
       <div className="container mx-auto flex justify-between items-center">
           <div className='menu h-screen w-full bg-primary-dark fixed z-10 top-0 left-0 md:hidden hidden'>
               <i className="fa-solid fa-xmark absolute top-4 right-4 text-2xl" onClick={showMenu}></i>
@@ -20,7 +21,7 @@ function Header() {
           <nav className='hidden md:block'>
               <Navigation dnone='md:inline-block' />
           </nav>
-          <i class="fa-solid fa-bars text-2xl md:hidden" onClick={showMenu}></i>
+          <i className="fa-solid fa-bars text-2xl md:hidden" onClick={showMenu}></i>
       </div>
     </header>
   )
